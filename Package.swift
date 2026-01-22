a352af5a5ed4c3b4dce3f8c59fe791670f400d27
scchn <scxchn@gmail.com>
2026-01-22 11:58:32 +0800

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "StarPRNTSDK",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "StarPRNTSDK",
+            targets: ["StarPRNTSDK"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "StarPRNTSDK",
+            dependencies: [
+                "StarIO",
+                "StarIO_Extension"
+            ],
+            path: "Sources/StarPRNTSDK",
+            publicHeadersPath: "."
+        ),
+        .binaryTarget(
+            name: "StarIO",
+            path: "software/libs/StarIO.xcframework"
+        ),
+        .binaryTarget(
+            name: "StarIO_Extension",
+            path: "software/libs/StarIO_Extension.xcframework"
+        )
+    ]
+)
---
+++ Sources/StarPRNTSDK/StarPRNTSDK.swift
@@ -0,0 +1,12 @@
+//
+//  StarPRNTSDK.swift
+//  StarPRNT SDK
+//
+//  This file re-exports the StarIO frameworks for Swift Package Manager
+//
+
+import Foundation
+
+// Re-export the frameworks so users only need to import StarPRNTSDK
+@_exported import StarIO
+@_exported import StarIO_Extension
